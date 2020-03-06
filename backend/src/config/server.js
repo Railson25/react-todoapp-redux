0d70dac3a3a21c3d6d195126fb78bee4289ac830
Railson25 <railsonsantiago123@gmail.com>
2020-03-06 12:52:23 -0300

    Habilitando Cors e criando Middleware

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,11 +4,12 @@ require('dotenv/config');
 
 const bodyParser = require('body-parser');
 const express = require('express');
-
 const server = express();
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors)
 
 server.listen(process.env.PORT, function() {
   console.log(`BACKEND is running on port ${process.env.PORT}`);
