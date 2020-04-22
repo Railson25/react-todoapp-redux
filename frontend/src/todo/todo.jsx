d0c6c8707a7f155b853e7217412cba363df6c835
Railson25 <railsonsantiago123@gmail.com>
2020-04-21 22:48:53 -0300

    Melhorias CSS e Limpar Formulário

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPanding = this.handleMarkAsPanding.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -58,6 +59,10 @@ export default class Todo extends Component {
             .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render() {
         return (
             <div>
@@ -66,6 +71,7 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
                     handleAdd={this.handleAdd}/>
               <TodoList 
                     list={this.state.list}
