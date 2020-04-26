1c91cd7642259bb72480fc6cf00dcc809f23dd8f
Railson25 <railsonsantiago123@gmail.com>
2020-04-26 20:43:55 -0300

    Action Creator changeDescription

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
 const TodoForm = props => {
     const keyHandler = (e) => {
@@ -18,7 +21,7 @@ const TodoForm = props => {
                 <Grid cols='12 9 10' >
                     <input id='description' className='form-control'
                         placeholder='Adicione uma tarefa'
-                        onChange={props.handleChange} 
+                        onChange={props.changeDescription} 
                         onKeyUp={keyHandler}
                         value={props.description} />
                 </Grid>
@@ -37,5 +40,6 @@ const TodoForm = props => {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
 
-export default connect(mapStateToProps)(TodoForm)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
