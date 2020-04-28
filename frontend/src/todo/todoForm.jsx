9b8ef5c3abef711660d7d803b752945bfc3a01d6
Railson25 <railsonsantiago123@gmail.com>
2020-04-28 11:50:53 -0300

    Adicionar Todo

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { changeDescription, search } from './todoActions'
+import { add, changeDescription, search } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -17,13 +17,15 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
+        const { add, search, description } = this.props
         if (e.key === 'Enter') {
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+            e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
             this.props.handleClear()
         }
     }
     render() {
+        const { add, search, description } = this.props
         return (
             <div role='form' className='todoForm' className="d-flex bd-highlight" >
             <div className="p-2 flex-fill bd-highlight">
@@ -38,9 +40,9 @@ class TodoForm extends Component {
     
             <Grid cols='12 3 2'>
                 <IconButton style='primary' icon='plus'
-                    onClick={this.props.handleAdd} />
+                    onClick={() => add(description)} />
                 <IconButton style='info' icon='search'
-                    onClick={this.props.handleSearch} /> 
+                    onClick={() => search()} /> 
                 <IconButton style='default' icon='close'
                     onClick={this.props.handleClear} />      
             </Grid>
@@ -51,6 +53,6 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
